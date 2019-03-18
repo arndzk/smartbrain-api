e22e55948d55f3fe01bb7f22a4129ed15af63432
CodexAcquillae <arn.dziudzik@gmail.com>
2019-03-18 16:11:58 +0200

    Added User Profile Recovery and Entry Update from Database

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.use(bodyParser.json());
 } */
 
 app.get('/', (req, res) => {
-    res.send(database.users);
+    res.send('Connected!');
 });
 
 app.post('/signin', (req, res) => {
@@ -83,31 +83,25 @@ app.post('/register', (req, res) => {
 
 app.get('/profile/:id', (req, res) => {
     const { id } = req.params;
-    let found = false;
-    database.users.forEach(user => {
-        if (user.id === id) {
-            found = true;
-            return res.json(user);
+    db.select('*').from('users').where({id}).then(user => {
+        if(user.length) {
+            res.json(user[0])
+        } else {
+            res.status(400).json('User not found!')
         }
     })
-    if (!found) {
-        res.status(400).json('User not found!')
-    }
+    .catch(err => res.status(400).json('Error Getting User!'))
 })
 
 app.put('/image', (req, res) => {
     const { id } = req.body;
-    let found = false;
-    database.users.forEach(user => {
-        if (user.id === id) {
-            found = true;
-            user.entries++;
-            return res.json(user.entries);
-        }
+    db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+        res.json(entries[0])
     })
-    if (!found) {
-        res.status(400).json('User not found!')
-    }
+    .catch(err => res.status(400).json('Error Getting Entries!'))
 })
 
 /* bcrypt.hash("bacon", null, null, function(err, hash) {
